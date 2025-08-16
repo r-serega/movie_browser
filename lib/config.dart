6c8d0a5438d5c980028337953c7fb9189e158b3b
Serhii Rudchenko <sergej.rudchenko@gmail.com>
2025-08-16 17:06:38 +0300

    fix release

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   final String baseUrl = 'https://api.themoviedb.org/3/';
-  final String apiKey = 'eb2c4f7c7b99459a967a4bd9d2b25c27';
+  final String apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
   final String bearer =
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjJjNGY3YzdiOTk0NTlhOTY3YTRiZDlkMmIyNWMyNyIsIm5iZiI6MTc1NTE5MzQ4NC40MDIsInN1YiI6IjY4OWUyMDhjMWMzZTdlMjZiZGI3MTU0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.beTB0HqXOpAZ3Pn1NCeTMgJ4HgOuTPFV-QsCKM-T1js';
+      'Bearer yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy';
 }
